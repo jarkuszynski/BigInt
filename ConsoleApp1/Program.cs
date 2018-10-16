5a7e21a4d75463a1771ade7380c763394fc2d2b8
jarkuszynski <37298865+jarkuszynski@users.noreply.github.com>
2018-10-16 21:14:14 +0200

    Added MyBigInteger, added method Add

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[] {9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
+            int[] number2 = new int[]{1,1,1,1};
+            MyBigInteger bigInt = new MyBigInteger(number);
+            bigInt.BigInt = bigInt.Add(number2);
+        }
+    }
+}
